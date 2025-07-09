743a4c7b7fc4f5c922ff2b9d5b57197b586c4889
Matheus <matheusguimaraes9262@gmail.com>
2025-07-08 21:40:57 -0400

    mudanca

--- src/app/lancamentos/opcoes/index.tsx
+++ src/app/lancamentos/opcoes/index.tsx
@@ -747,7 +747,7 @@ export default function Opcoes({ informacoesEmprestimo }: PropsAlterar ) {
               </div>
 
 
-              <button type="submit" className="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg text-center cursor-pointer w-full9 bg-[linear-gradient(90deg,_rgba(4,128,8,1)_1%,_rgba(0,125,67,1)_50%,_rgba(10,115,5,1)_100%)] hover:bg-[linear-gradient(90deg,_rgba(6,150,10,1)_1%,_rgba(0,145,77,1)_50%,_rgba(12,135,7,1)_100%)] transition duration-200 my-4"> Salvar </button>
+              <button type="submit" className="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg text-center cursor-pointer w-full h-9 bg-[linear-gradient(90deg,_rgba(4,128,8,1)_1%,_rgba(0,125,67,1)_50%,_rgba(10,115,5,1)_100%)] hover:bg-[linear-gradient(90deg,_rgba(6,150,10,1)_1%,_rgba(0,145,77,1)_50%,_rgba(12,135,7,1)_100%)] transition duration-200 my-4"> Salvar </button>
 
               <div className="flex justify-center gap-4">
                 <button 
